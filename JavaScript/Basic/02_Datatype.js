a48db474dd63b87d49d6b315d7c48a4ed2d28a8b
nikhildubey-23 <ravannick0089@gmail.com>
2024-12-07 05:23:36 +0530

    data types

---
+++ JavaScript/Basic/02_Datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js as newer version
+
+//alert("hello world") //we are using node not browser 
+
+let name = "nikhil"
+let age = 20
+let isLoggedIn = false
+
+//number ==> 2 to power 53
+//bigint
+//string = ""
+//null ==> standalone value 
+//undefine 
+//symbol ==> mostly use for uniqueness
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
