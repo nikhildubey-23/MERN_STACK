3b83245e11654526c221bae263266d4fe2bbbb10
nikhildubey-23 <ravannick0089@gmail.com>
2024-12-12 14:19:43 +0530

    completing the hero section

--- Mini-Project-in-TailwindCss/tailwind.config.js
+++ Mini-Project-in-TailwindCss/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-in': {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        'slide-in-reverse': {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        'slide-in': 'slide-in 1s ease-in-out 0.5s forwards',
+        'slide-in-reverse': 'slide-in-reverse 1s ease-in-out 0.5s forwards',
+      },
+    },
   },
   plugins: [],
 }
