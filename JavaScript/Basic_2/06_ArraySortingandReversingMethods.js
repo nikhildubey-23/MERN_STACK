7d37d9d33db0f3cd767ecdb86b5c8008a7db204c
nikhildubey-23 <ravannick0089@gmail.com>
2024-12-23 09:55:34 +0530

    aray shorting and reversing practice done and also practiced the question

--- JavaScript/Basic_2/06_ArraySortingandReversingMethods.js
+++ JavaScript/Basic_2/06_ArraySortingandReversingMethods.js
@@ -1,2 +1,29 @@
 // sort()
 // reverse()
+
+
+//sort method :- The sort() method modifies the original array.
+
+// The sort() method returns the sorted array.
+
+// The default sort order is according to the string Unicode code points.
+
+// The sort() method can take a compare function as an argument to customize the sort order.
+
+//  The sort() method is not stable, meaning that equal elements may not keep their original order.
+
+const number = [4, 2, 7, 1, 3, 3, 1];
+number.sort()
+console.log(number)
+
+//reverse():- The reverse() method modifies the original array.
+
+// The reverse() method returns the reversed array.
+
+// The reverse() method reverses the order of the elements in the array, but does not change the values of the elements.
+
+number.reverse()
+console.log(number)
+
+
+
---
+++ JavaScript/Basic_2/06_ArraySortingandReversingMethodsPracticeQuestion.js
@@ -0,0 +1,20 @@
+// Write a JavaScript statement to sort the elements of the array [3, 1, 2, 4] in ascending order.
+
+const num = [3,1,2,4]
+num.sort()
+console.log(num)
+
+
+// // Write a JavaScript statement to reverse the order of the elements in the array [1, 2, 3, 4, 5].
+
+const num2 = [1, 2, 3, 4, 5]
+num2.reverse()
+console.log(num2)
+
+// // // What is the output of the following code: let numbers = [4, 2, 7, 1, 3]; numbers.sort(); console.log(numbers);
+
+// output is [1,2,3,4,7]
+
+// // // What is the output of the following code: let numbers = [1, 2, 3, 4, 5]; numbers.reverse(); console.log(numbers);
+
+// output is [5,4,3,2,1]
