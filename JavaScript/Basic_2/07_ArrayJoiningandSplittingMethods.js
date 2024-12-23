5fe12b378aab22d6f7dd2b4a5ac5b7c29a698c16
nikhildubey-23 <ravannick0089@gmail.com>
2024-12-23 12:09:15 +0530

    join and split method also done with its practice question

--- JavaScript/Basic_2/07_ArrayJoiningandSplittingMethods.js
+++ JavaScript/Basic_2/07_ArrayJoiningandSplittingMethods.js
@@ -1,2 +1,31 @@
 // join()
 // split()
+
+// join()
+
+//The join() method concatenates all the elements of an array into a single string.
+
+// The join() method returns the concatenated string.
+
+// The join() method does not modify the original array.
+
+// The join() method can take an optional separator string as an argument. If no separator is provided, the elements are joined with no separator.
+
+
+const array = [1,2,4,6,63,234]
+const string = array.join()
+console.log(`the class of array is now ${typeof(array)} and its output is ${string}`)
+
+// split()
+// The split() method splits a string into an array of substrings.
+
+// // The split() method returns the array of substrings.
+
+// // The split() method does not modify the original string.
+
+// // // // The split() method can take an optional separator string as an argument. If no separator is provided, the entire string is returned as a single element in the array.
+
+const str = "hello world"
+const arr = str.split(" ")
+console.log(arr)
+console.log(typeof(arr))
---
+++ JavaScript/Basic_2/07_ArrayJoiningandSplittingMethodsPracticeQuestion.js
@@ -0,0 +1,22 @@
+// Write a JavaScript statement to concatenate all the elements of the array ["hello", "world", "JavaScript"] into a single string with the separator " ".
+
+const array = ['hello',"world","Javascript"]
+const string = array.join(" ")
+console.log(string)
+
+// // // Write a JavaScript statement to split the string "hello world" into an array of substrings with the separator " ".
+
+const arr2 = "hello world"
+const realarray = arr2.split("")
+console.log(realarray)
+
+
+// // // What is the output of the following code: let sentence = "hello world"; let words = sentence.split(""); console.log(words);
+
+// output :- [
+//     'h', 'e', 'l', 'l',
+//     'o', ' ', 'w', 'o',
+//     'r', 'l', 'd'
+//   ]
+
+
